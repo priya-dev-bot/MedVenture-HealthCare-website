e8adccaa0ac742350cfb15ff127057cf788ec1f8
Priyanka Jatoliya <priyankajatoliya10@gmail.com>
2026-01-15 15:54:36 +0530

    Initial project commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -51,7 +51,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-card">
-        <h2>Welcome Back To MedVenture <span>🎉😊</span></h2>
+        <h2>Welcome Back To MedVenture! <span>🎉😊</span></h2>
         <form onSubmit={handleSubmit}>
           <input 
             type="email" 
